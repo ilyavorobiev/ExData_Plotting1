16671b9fccbc99120533d7c6727cd4c477a86208
Vorobiev Ilya <vorobei99@gmail.com>
2016-02-23 01:56:09 +0300

    Added code for plot1 and its png

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# Download and unzip data
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp, mode="wb")
+unzip(temp, "household_power_consumption.txt")
+
+# Read data skipping dates. We only need 1/2/2007 and 2/2/2007 
+consumption <- read.csv.sql("household_power_consumption.txt", sql = 'select * from file where Date = "1/2/2007" or Date = "2/2/2007"', sep = ";")
+
+# Format data
+consumption[consumption == "?"] <- NA
+consumption$DateTime <- as.POSIXct(paste(consumption$Date, consumption$Time), format="%d/%m/%Y %H:%M:%S")
+
+# Create histogram
+hist(consumption$Global_active_power,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+
+# Copy to file
+dev.copy(png, file = "plot1.png",height=480,width=480)
+dev.off()
